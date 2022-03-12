d0ffb0e0efdd8a81aee4cf383ba43eb875c02f6a
Enrick Santos de Souza <enricksantos03@gmail.com>
2022-03-12 05:37:54 -0300

    v2 - App Bottom Layer

---
+++ model/data.js
@@ -0,0 +1,39 @@
+const songs = [
+  {
+    id: 1,
+    title: '19th Floor',
+    artist: 'Bobby Richards',
+    artwork: require('../assets/img/img1.jpg'),
+    url: require('../assets/audio/19th Floor - Bobby Richards.mp3'),
+  },
+  {
+    id: 2,
+    title: 'Awful',
+    artist: 'josh pan',
+    artwork: require('../assets/img/img2.jpg'),
+    url: require('../assets/audio/Awful - josh pan.mp3'),
+  },
+  {
+    id: 3,
+    title: 'Something is Going On',
+    artist: 'Godmode',
+    artwork: require('../assets/img/img3.jpg'),
+    url: require('../assets/audio/Something is Going On - Godmode.mp3'),
+  },
+  {
+    id: 4,
+    title: 'Book The Rental Wit It',
+    artist: 'RAGE',
+    artwork: require('../assets/img/img4.jpg'),
+    url: require('../assets/audio/Book The Rental Wit It - RAGE.mp3'),
+  },
+  {
+    id: 5,
+    title: 'Crimson Fly',
+    artist: 'Huma-Huma',
+    artwork: require('../assets/img/img5.jpg'),
+    url: require('../assets/audio/Crimson Fly - Huma-Huma.mp3'),
+  },
+];
+
+export default songs;
--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -1,12 +1,45 @@
 import React from 'react'
-import { View, Text, StyleSheet, SafeAreaView  } from 'react-native'
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    SafeAreaView, 
+    TouchableOpacity,
+    Dimensions
+} from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+
+const {width, height} = Dimensions.get('window')
 
 const MusicPlayer = () => {
   return (
     <SafeAreaView style={style.container}>
         <View style={style.maincontainer}>
-        {/* image */}
+            {/* image */}
+
+            {/* slider */}
+
+            {/* music controls */}
+        </View>
+
+        <View style={style.bottomContainer}>
+            <View style={style.bottomIconContent}>
+                <TouchableOpacity onPress={() => {}}>
+                    <Ionicons name="heart-outline" size={30} color="#888888" />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => {}}>
+                    <Ionicons name="repeat" size={30} color="#888888" />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => {}}>
+                    <Ionicons name="share-outline" size={30} color="#888888" />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => {}}>
+                    <Ionicons name="ellipsis-horizontal" size={30} color="#888888" />
+                </TouchableOpacity>
+            </View>
         </View>
     </SafeAreaView>
   )
@@ -17,11 +50,23 @@ export default MusicPlayer
 const style = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#555',
+        backgroundColor: '#222831',
     },
     maincontainer: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    bottomContainer: {
+        width: width,
+        alignItems: 'center',
+        paddingVertical: 15,
+        borderTopColor: '#393E46',
+        borderTopWidth: 1,
+    },
+    bottomIconContent: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '80%',
     },
 })
