593cf4bd8107ae15349c0f0fa002e2a5b946501e
Enrick Santos de Souza <enricksantos03@gmail.com>
2022-03-12 07:15:53 -0300

    v3 - App Main Container_UI Complete

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StatusBar, StyleSheet } from 'react-native'
 import MusicPlayer from './screens/MusicPlayer'
 import { Component } from 'react/cjs/react.development'
 
 const App = () => {
   return (
     <View style={style.container}>
+      <StatusBar barStyle='black-content' />
       <MusicPlayer />
     </View>
   )
--- screens/MusicPlayer.js
+++ screens/MusicPlayer.js
@@ -5,8 +5,10 @@ import {
     StyleSheet, 
     SafeAreaView, 
     TouchableOpacity,
-    Dimensions
+    Dimensions,
+    Image
 } from 'react-native'
+import Slider from '@react-native-community/slider';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const {width, height} = Dimensions.get('window')
@@ -16,10 +18,54 @@ const MusicPlayer = () => {
     <SafeAreaView style={style.container}>
         <View style={style.maincontainer}>
             {/* image */}
+            <View style={[style.imageContent, style.elevation]}>
+                <Image
+                source={require('../assets/img/img1.jpg')}
+                style={style.musicImage}
+                />
+            </View>
+
+            {/* song content */}
+            <View>
+                <Text style={[style.songContent, style.songTitle]}>Some Title</Text>
+                <Text style={[style.songContent, style.songArtist]}>Some Artist</Text>
+            </View>
 
             {/* slider */}
+            <View>
+                <Slider
+                style={style.progressBar}
+                value={10}
+                minimumValue={0}
+                maximumValue={100}
+                thumbTintColor="#FFD369"
+                minimumTrackTintColor="#FFD369"
+                maximumTrackTintColor="#fff"
+                onSlidingComplete={() => {}}
+                />
+
+                {/* music progress durations */}
+                <View style={style.progressLevelDuration}>
+                    <Text style={style.progressLabelText}>00:00</Text>
+                    <Text style={style.progressLabelText}>00:00</Text>
+                </View>
+            </View>
 
             {/* music controls */}
+            <View style={style.musicControlsContainer}>
+                <TouchableOpacity onPress={() => {}}>
+                    <Ionicons name="play-skip-back-outline" size={35} color="#FFD369" />
+                </TouchableOpacity>
+                
+                <TouchableOpacity onPress={() => {}}>
+                    <Ionicons name="ios-pause-circle" size={75} color="#FFD369" />
+                </TouchableOpacity>
+                
+                <TouchableOpacity onPress={() => {}}>
+                    <Ionicons name="play-skip-forward-outline" size={35} color="#FFD369" />
+                </TouchableOpacity>
+            </View>
+
         </View>
 
         <View style={style.bottomContainer}>
@@ -68,5 +114,62 @@ const style = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         width: '80%',
+    },
+    imageContent: {
+        width: 300,
+        height: 340,
+        marginBottom: 20,
+        marginTop: 20,
+    },
+    musicImage:  {
+        width: '100%',
+        height: '100%',
+        borderRadius: 15,
+    },
+
+    elevation: {
+        elevation: 5,
+
+        shadowColor: '#ccc',
+        shadowOffset: {
+            width: 5,
+            height: 5,
+        },
+        shadowOpacity: 0.5,
+        shadowRadius: 3.84,
+    },
+    songContent: {
+        textAlign: 'center',
+        color: '#EEEEEE',
+    },
+    songTitle: {
+        fontSize: 18,
+        fontWeight: '600',
+    },
+    songArtist: {
+        fontSize: 16,
+        fontWeight: '300',
+    },
+    progressBar: {
+        width: 350,
+        height: 40,
+        marginTop: 20,
+        flexDirection: 'row',
+    },
+    progressLevelDuration: {
+        width: 340,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    progressLabelText: {
+        color: '#eee',
+        fontWeight: '500',
+    },
+    musicControlsContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '60%',
+        marginTop: 10,
     },
 })
